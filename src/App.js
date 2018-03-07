b899c1a49fdb4779387394741797543cc44d3efd
Tharit Pongsaneh <tharit.p@ku.th>
2018-03-07 21:22:41 +0700

    First

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <p>hellow world</p>
+        <h1>HELLOWOWTLF</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
